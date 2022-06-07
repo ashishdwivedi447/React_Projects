8c1efbf87b1f57fbaf4f69e195da2fc403e874a9
sarikasingh30 <97461783+sarikasingh30@users.noreply.github.com>
2022-06-07 12:29:15 +0530

    commit

--- restaurant1/src/components/Show/ShowDetails.jsx
+++ restaurant1/src/components/Show/ShowDetails.jsx
@@ -20,33 +20,50 @@ const ShowDetails = () => {
         setTotalCount(Number(response.headers["x-total-count"]))
       });
   }
-  // const handleChange=(e)=>{
-  //   if(e.target.value=="High"){
-  //     console.log(e.target.value)
-  //     let x=rest.sort((a,b)=>{return (b.price-a.price)})
-  //     setRest(x)
-  //   }
-  //   else if(e.target.value=="Low"){
-  //     console.log(e.target.value)
-  //     let x=rest.sort((a,b)=>{console.log(typeof(a.price),b.price)})
-  //     setRest(x)
-  //   }
-  // }
-  const handleStar=(e)=>{
-
+  const handleChange=(e)=>{
+    if(e.target.value=="High"){
+      console.log(e.target.value)
+      let x=rest.sort((a,b)=>{return (b.price-a.price)})
+      console.log(x)
+      setRest([...x])
+    }
+    else if(e.target.value=="Low"){
+      console.log(e.target.value)
+      let x=rest.sort((a,b)=>{return (a.price-b.price)})
+      setRest([...x])
+    }
+  }
+  const handleStar1=(e)=>{
     console.log(e.target.value)
+    // if(e.target.value==="1star"){
+    // let y=rest.filter((k)=>(k.rating>=1&&k<2))
+    // setRest(y)
+    // }
+  }
+  const handleStar2=(e)=>{
+    // let y=rest.filter((k)=>k.rating>=2&&k<3)
+    // setRest(y)
+  }
+  const handleStar3=(e)=>{
+    // let y=rest.filter((k)=>k.rating>=3&&k<4)
+    // setRest(y)
+  }
+  const handleStar4=(e)=>{
+    // let y=rest.filter((k)=>k.rating>=4&&k<5)
+    // setRest(y)
   }
   const handleCash=(e)=>{
-    if(e.target.value="Cash"){
-      let y=rest.filter((e)=>e.card==false)
+    console.log(e.target.value)
+      let y=rest.filter((k)=>k.card===false)
       setRest(y)
-    }
-    else if(e.target.value="Card"){
-      let y=rest.filter((e)=>e.card==true)
-      setRest(y)
-    }
   }
-   
+  const handleCard=(e)=>{
+    console.log(e.target.value)
+      let b=rest.filter((k)=>k.card===true)
+      setRest(b)
+    
+  }
+
  
  
   return (
@@ -56,14 +73,14 @@ const ShowDetails = () => {
           <option value="High">High to low</option>
           <option value="Low">Low to High</option>
         </select>
-        <button value="1star" onClick={handleStar} >1 Star</button>
-        <button onClick={handleStar}  value='2star' >2 Star</button>
-        <button  onClick={handleStar} value='3star'>3 Star</button>
-        <button onClick={handleStar}  value="4star">4 Star</button>
+        <button value="1star" onClick={handleStar1} >1 Star</button>
+        <button onClick={handleStar2}  value='2star' >2 Star</button>
+        <button  onClick={handleStar3} value='3star'>3 Star</button>
+        <button onClick={handleStar4}  value="4star">4 Star</button>
         
         
-        <button onClick={handleCash} value='Cash'>Cash Only</button>
-        <button onClick={handleCash} value='Card'>Card Only</button>
+        <button value="cash" onClick={handleCash} >Cash Only</button>
+        <button value="card" onClick={handleCard} >Card Only</button>
       </div>
       <div className={styles.restaurant}>
         {rest.map((e)=>{
@@ -71,7 +88,7 @@ const ShowDetails = () => {
             <div key={e.id}>
               <img className={styles.imagep} src={e.image} alt="rest"/>
               <div className={styles.headdiv}>
-                {(e.card==true)?<FaCcMastercard size="3em"/>:<FaMoneyBillAlt size="3em"/>}
+                {(e.card===true)?<FaCcMastercard size="3em"/>:<FaMoneyBillAlt size="3em"/>}
                 <div >Ratings: {e.rating} </div>
                 <FaStar size="2em"/>
               </div>
